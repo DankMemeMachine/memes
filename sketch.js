962d023aed3f6153a5c959613971607502054f7d
dank_memes.jpg <63226@bwls.net>
2016-10-18 10:31:23 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
 function setup() {
 createCanvas(windowWitdth, widowHeight);
 }
+var X=10
 
-function draw() {
+draw=function() {
+  background(22,225,225)
+  // draw the car body
+fill(255, 0, 115);
+rect(x, 200, 100, 20);
+rect(x + 15, 178, 70, 40);
 
-}
+// draw the wheels
+fill(77, 66, 66);
+ellipse(x + 25, 221, 24, 24);
+ellipse(x + 75, 221, 24, 24);
+  x=x+1
+};
